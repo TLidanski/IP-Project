442bd8c0d55edf14a02aef157e28a46f9e73248f
Tihomir <t_lidanski@abv.bg>
2016-03-31 13:53:12 +0300

    #6 Starting to develop the getIssues feature.

--- Bug_And_Issue_Tracker/WebContent/js/dashboard.js
+++ Bug_And_Issue_Tracker/WebContent/js/dashboard.js
@@ -5,8 +5,59 @@ $(document).ready(function() {
 
 	if ($.cookie('Session')) {
 		var userData = JSON.parse($.cookie('Session'));
-		$('#dashboard-profile').attr('src', userData[0].picture);
+
+		$('#dashboard-profile').attr('src', userData.picture);
+		$('.profile-usertitle-name').text(userData.fname + ' ' + userData.lname);
+		$('.profile-usertitle-job').text(userData.email);
+
+		$.ajax({
+			url: _url + '/issues',
+			dataType: 'json',
+			data: {
+				userId: userData.id
+			},
+			success: function(data) {
+				if (data.length != 0) {
+					$('#issues').append('<div class="list-group" id="issue-list"></div>');
+					$.each(data, function(i, val) {
+						var glyph = (val.type == 'task') ? '<i class="fa fa-wrench"></i>' : 
+							'<i class="fa fa-bomb"></i>';
+
+						$('#issue-list').append('<button class="list-group-item" id="'+val.id+'">'+
+							glyph + ' ' + val.issueTitle +'</button>'
+						);
+					});
+				} else {
+					$('#issues').html('<img src="../images/rabota.jpg">');
+				}
+			}
+		});
+
+		$(document).on('click', '.list-group-item', function() {
+			$('#issueModal').modal();
+			$('#issueModal .modal-title').text($(this).text());
+			getDataByIssueId('/issueComments', this.id).done(function() {
+
+			});
+			getDataByIssueId('/subTasks', this.id).done(function(data) {
+				console.log(data);
+				$.each(data, function(i, val) {
+					$('#subTaskList').append('<li>'+val.title+'</li>');
+				});
+			});
+		});
+
+		// $(document).on('hidden.bs.modal', '#issueModal', function() {
+		// 	$(this).empty();
+		// });
+
 	} else {
 		window.location.href = _home + '/index.html';
+	}
+
+	function getDataByIssueId(url, id, data) {
+		return $.get(_url + url, {
+			issueId: id
+		});
 	}
 });
